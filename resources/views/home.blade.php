598f8879e781b3d2f833acaec75df5e93d7a2eec
michelleangela19 <misel.angela79@gmail.com>
2023-03-29 16:51:51 +0700

    navbar awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Michelle's Profile</title>
+    <link rel="shortcut icon" href="media/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="node_modules/@popperjs/core/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+        <div class="container-fluid container">
+            <a class="navbar-brand" href="#Home">
+                <img src="media/logo.png" alt="Bootstrap" width="80" height="auto">
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="#Home">HOME</a>
+                    <a class="nav-link" href="#About">ABOUT ME</a>
+                    <a class="nav-link" href="#Contact">CONTACT</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0" style=>
+        <h4 id="Home">Item 1</h4>
+        <h5 id="About">Item 1-1</h5>
+        <h5 id="Contact">Item 1-2</h5>
+        
+    </div>
+</body>
+</html>
