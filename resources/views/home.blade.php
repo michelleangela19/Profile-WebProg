d08d42234a3400d8bcffc535666ace75da2d86b8
michelleangela19 <misel.angela79@gmail.com>
2023-04-02 17:48:08 +0700

    Final banget

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -113,62 +113,63 @@
             </div>
 
 
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <h3 class="skill-title">My Skills</h3>
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <h3 class="skill-title">My Skills</h3>
+            <div class="skill-container">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="media/slide 1.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>HTML, CSS, JavaScript</h2>
+                                <h5>FrontEnd Development Program</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/slide 2.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>MySQL, C++, Python</h2>
+                                <h5>BackEnd Development Program </h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/slide 3.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Canva</h2>
+                                <h5>Graphic Design Tool</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/slide 4.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Capcut</h2>
+                                <h5>Video Editing Tool</h5>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="media/slide 5.png" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h2>Word, Excel, Powerpoint</h2>
+                                <h5>Microsoft Programs</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="media/slide 1.png" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2>HTML, CSS, JavaScript</h2>
-                            <h5>FrontEnd Development Program</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="media/slide 2.png" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2>MySQL, C++, Python</h2>
-                            <h5>BackEnd Development Program </h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="media/slide 3.png" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2>Canva</h2>
-                            <h5>Graphic Design Tool</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="media/slide 4.png" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2>Capcut</h2>
-                            <h5>Video Editing Tool</h5>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="media/slide 5.png" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h2>Word, Excel, Powerpoint</h2>
-                            <h5>Microsoft Programs</h5>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
-        </div>
 
         
         <h2 class="and font">And many more to know..</h2>
