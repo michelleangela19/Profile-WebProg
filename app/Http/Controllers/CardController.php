e2bd3ae74c10181d22f201a095e0e52947d6da0a
michelleangela19 <misel.angela79@gmail.com>
2023-04-29 23:24:11 +0700

    ganti bahasa

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -10,10 +10,10 @@ class CardController extends Controller
         $itemInfo = [
             ['image' => 'bakpao.jpg', 'name' => 'Salted Egg Steamed Bun', 'desc' => 'Steam buns with salted egg filling', 'status' => 'Very Like', 'rating' => '8'],
             ['image' => 'cheesecake.jpg', 'name' => 'Cheesecake', 'desc' => 'Cake with a soft textured cheese base', 'status' => 'Like', 'rating' => '7.8'],
-            ['image' => 'fruit.jpg', 'name' => 'Buah-buahan', 'desc' => 'Just fruit..', 'status' => 'Very Like', 'rating' => '8.9'],
-            ['image' => 'ice cream.jpg', 'name' => 'Es Krim', 'desc' => 'Dairy products that are frozen and mixed with flavors and sweeteners', 'status' => 'So Much Like', 'rating' => '9.5'],
-            ['image' => 'pho.jpg', 'name' => 'Pho Sapi', 'desc' => 'Noodle soup with beef broth, vegetables and sliced ​​beef', 'status' => 'So Much Like', 'rating' => '9.8'],
-            ['image' => 'siomay.jpg', 'name' => 'Siomay Dimsum', 'desc' => 'Dimsum of steamed pork or shrimp wrapped in wonton skin', 'status' => 'Like', 'rating' => '7']
+            ['image' => 'fruit.jpg', 'name' => 'Fruits', 'desc' => 'Just fruit..', 'status' => 'Very Like', 'rating' => '8.9'],
+            ['image' => 'ice cream.jpg', 'name' => 'Ice Cream', 'desc' => 'Dairy products that are frozen and mixed with flavors and sweeteners', 'status' => 'So Much Like', 'rating' => '9.5'],
+            ['image' => 'pho.jpg', 'name' => 'Beef Pho', 'desc' => 'Noodle soup with beef broth, vegetables and sliced ​​beef', 'status' => 'So Much Like', 'rating' => '9.8'],
+            ['image' => 'siomay.jpg', 'name' => 'Dimsum Shumai', 'desc' => 'Dimsum of steamed pork or shrimp wrapped in wonton skin', 'status' => 'Like', 'rating' => '7']
         ];
         return view('home', compact('itemInfo'));
     }
@@ -22,10 +22,10 @@ class CardController extends Controller
         $itemInfo = [
             ['image' => 'bakpao.jpg', 'name' => 'Salted Egg Steamed Bun', 'desc' => 'Steam buns with salted egg filling', 'status' => 'Very Like', 'rating' => '8'],
             ['image' => 'cheesecake.jpg', 'name' => 'Cheesecake', 'desc' => 'Cake with a soft textured cheese base', 'status' => 'Like', 'rating' => '7.8'],
-            ['image' => 'fruit.jpg', 'name' => 'Buah-buahan', 'desc' => 'Just fruit..', 'status' => 'Very Like', 'rating' => '8.9'],
-            ['image' => 'ice cream.jpg', 'name' => 'Es Krim', 'desc' => 'Dairy products that are frozen and mixed with flavors and sweeteners', 'status' => 'So Much Like', 'rating' => '9.5'],
-            ['image' => 'pho.jpg', 'name' => 'Pho Sapi', 'desc' => 'Noodle soup with beef broth, vegetables and sliced ​​beef', 'status' => 'So Much Like', 'rating' => '9.8'],
-            ['image' => 'siomay.jpg', 'name' => 'Siomay Dimsum', 'desc' => 'Dimsum of steamed pork or shrimp wrapped in wonton skin', 'status' => 'Like', 'rating' => '7']
+            ['image' => 'fruit.jpg', 'name' => 'Fruits', 'desc' => 'Just fruit..', 'status' => 'Very Like', 'rating' => '8.9'],
+            ['image' => 'ice cream.jpg', 'name' => 'Ice Cream', 'desc' => 'Dairy products that are frozen and mixed with flavors and sweeteners', 'status' => 'So Much Like', 'rating' => '9.5'],
+            ['image' => 'pho.jpg', 'name' => 'Beef Pho', 'desc' => 'Noodle soup with beef broth, vegetables and sliced ​​beef', 'status' => 'So Much Like', 'rating' => '9.8'],
+            ['image' => 'siomay.jpg', 'name' => 'Dimsum Shumai', 'desc' => 'Dimsum of steamed pork or shrimp wrapped in wonton skin', 'status' => 'Like', 'rating' => '7']
         ];
 
         $chosen = [];
